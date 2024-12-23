d813c9f89ddec02e5082dcabac3f95b6c034e25d
jiangning <jiangning198511@gmail.com>
2024-12-23 14:22:54 +0800

    添加文件

---
+++ sub.f90
@@ -0,0 +1,8 @@
+module hello
+        implicit none
+contains
+        subroutine sub()
+                write(*,*) "hello, world !"
+                return
+         end subroutine
+end module hello
